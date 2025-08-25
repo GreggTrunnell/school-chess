9e7db67989c8e7f85cd56cbf49b05954723854f2
Gregg Trunnell <gggtrunnell@gmail.com>
2025-08-25 17:30:07 -0500

    added chess images

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,33 @@
+import { useState, useEffect } from "react";
 import Nav from "../Nav/Nav";
 
 function Header() {
+  const images = [
+    "https://schoolchess.org/old/images/primary_champ_2025.jpeg?x=20250506150359",
+    "https://schoolchess.org/old/images/elementary_champ_2025.jpeg?x=20250506155801",
+    "https://schoolchess.org/old/images/middle_champ_2025.jpeg?x=20250506160128",
+    "https://schoolchess.org/old/images/coachoftheyear2025.jpeg?x=20250506161611",
+  ];
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 2000); // change every 2 seconds
+
+    return () => clearInterval(interval); // cleanup
+  }, [images.length]);
 
   return (
-    <div className='Header'>
-      <div className="Header">
-        <h1 className="green-text">School Chess Association</h1>
-        <Nav />
-        <img src="public/images/night_time_court.webp" style={{ width: "400px", height: "375px" }} />
-      </div>
+    <div className="Header">
+      <h1 className="green-text">School Chess Association</h1>
+      <Nav />
+      <img
+        src={images[currentIndex]}
+        alt="Slideshow"
+        style={{ width: "400px", height: "375px", borderRadius: "12px" }}
+      />
     </div>
   );
 }

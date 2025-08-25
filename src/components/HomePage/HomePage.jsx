f5907f03e2c09ffef82b0dc2ef3576bc3f573d47
Gregg Trunnell <gggtrunnell@gmail.com>
2025-08-25 16:20:37 -0500

    cleared out unused components

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,52 +1,12 @@
-import useStore from '../../zustand/store';
-import { NavLink } from 'react-router-dom';
-import RecipientRequests from '../RecipientRequests/RecipientRequests';
+import { NavLink } from "react-router-dom";
 
-function HomePage() {
-  const user = useStore((state) => state.user);
-
+function HomePage( ) {
+  
   return (
-    <div>
-      <RecipientRequests />
-      <br />
-      <img
-        src="public/images/tennis_ball_guy.webp"
-        style={{ width: "325px", height: "300px" }}
-      />
-      <h3>Player Profile:</h3>
-      <div className="container mt-4">
-        <div className="col-md-7 mb-7">
-          <div
-            className="card shadow-sm"
-            style={{ backgroundColor: "#336c1f" }}
-          >
-            <div className="card-body">
-              <h5 className="maroon-text">{user.username}</h5>
-              <hr style={{ borderTop: "5px solid #800000" }} />
-              <p className="card-text">
-                <strong className="maroon-text">Email: </strong> {user.email}
-                <br />
-                <strong className="maroon-text">City: </strong> {user.city}
-                <br />
-                <strong className="maroon-text">Zip: </strong> {user.zip} <br />
-                <strong className="maroon-text">Greeting: </strong>
-                {user.greeting} <br />
-                <strong className="maroon-text">Playing Hand: </strong>
-                {user.playing_hand} <br />
-                <strong className="maroon-text">Racquet Brand: </strong>
-                {user.racquet_brand}
-              </p>
-              <div>
-                <li>
-                  <NavLink to="/UpdateProfile">Update Profile</NavLink>
-                </li>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+     <div className='HomePage'>
+      <h1>Hey this is the homepage!</h1>
     </div>
   );
-}
+  }
 
 export default HomePage;
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,34 +1,15 @@
 import { NavLink } from 'react-router-dom';
-import useStore from '../../zustand/store';
 
-
-function Nav() {
-  const user = useStore((store) => store.user);
-
-  return (
-
-  <nav>
-    <div className="btn-group" role="group">
-      { !user.id ? ( 
-        // Guest access
-        <>
-          <NavLink className="btn btn-success" to="/login">Login</NavLink>
-          <NavLink className="btn btn-secondary" to="/registration">Register</NavLink>
-        </>
-      ) : ( 
-        // User access
-        <>
-          <NavLink className="btn btn-success" to="/">Home</NavLink>
-          <NavLink className="btn btn-success" to="/find_a_friend">Find A Friend</NavLink>
-          <NavLink className="btn btn-success" to="/friends_page">Friends</NavLink>
-        </>
-      )}
+function Nav( ) {
   
-      {/* All access */}
-      <NavLink className="btn btn-success" to="/about">About</NavLink>
+  return (
+    <nav>
+     <div className='Nav'>
+    <NavLink to="/">Home</NavLink>
+     <NavLink to="/nav">Nav</NavLink>
     </div>
-  </nav>
+    </nav>
   );
-}
+  }
 
 export default Nav;

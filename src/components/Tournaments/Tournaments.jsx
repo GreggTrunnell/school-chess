329ddc92d4d14a516310900b2d49a5298736fe9a
Gregg Trunnell <gggtrunnell@gmail.com>
2025-08-27 21:30:32 -0500

    started writing objects for tournaments

--- src/components/Tournaments/Tournaments.jsx
+++ src/components/Tournaments/Tournaments.jsx
@@ -1,30 +1,43 @@
 
 
 function Tournaments() {
-
-  //I orginally did not use an array for the different sections. Changing it to a .map eliminates clutter in return statement, reads easier,
-  //much better for scalability. 
-  const tourneyList = [{
-     
+  const tourneyList = [
+    {
       id: "one",
-      title: "Special Tournaments",
-      tournament: [{
+      category: "Special Tournaments",
+      tournament: [
+      {
         title: "USCF Rated Chess",
         description: `Players in this section must be current members of the United States Chess Federation and bring their USCF membership card. 
         Check USCF website for information regarding fees. Memberships can be purchased at the tournament site. Bring chess clock and chess set as 
         there will be a limited number of chess sets available . All USCF rules are followed. Rated chess is available at all tournaments except 
-        the SCA Statewide Primary, Elementary, Middle Championship, April Open, and Grade Level Tournaments.` 
-      }],
-      show: false
-    ,
+        the SCA Statewide Primary, Elementary, Middle Championship, April Open, and Grade Level Tournaments.`,
+        show: false
+      },
+      {
+        title: "Regular Tournament",
+        description: `At our regular tournaments we offer four sections: Primary PK-2, Elementary PK-5, Middle PK-8, and USCF Rated 
+      PK-Adult. Younger players may always play up. Players in the USCF Rated PK-Adult section must either be or become a United States 
+      Chess Federation member.`,
+        show: false
+      }
+     ]
+    },
+    {
+      id:"two",
+      category: "Statewide Tournament",
+      tournament: [{
+        title: "SCA Statewide Championship",
+        description: `This is statewide. All sorts of levels`,
+        show: false
+      }]
+    }
 
-  }
-   
   ];
 
-return (
+  return (
     <div className="accordion" id="accordionExample">
-      {tourneyList.map(({ id, title, image, alt, strong, body, show }) => (
+      {tourneyList.map(({ id, category, tournament, alt, strong, body, show }) => (
         <div className="accordion-item" key={id}>
           <h2 className="accordion-header" id={`heading${id}`}>
             <button
@@ -35,7 +48,7 @@ return (
               aria-expanded={show}
               aria-controls={`collapse${id}`}
             >
-              {title}
+              {category}
             </button>
           </h2>
           <div
@@ -45,8 +58,8 @@ return (
             data-bs-parent="#accordionExample"
           >
             <div className="accordion-body tennis-green">
-              <img src={image}
-              alt={alt}/>
+              <img src={tournament}
+                alt={alt} />
               <strong>{strong}</strong> {body}
             </div>
           </div>
